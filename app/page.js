e0076895e729e2dea6d4e7fda6517fea519fdc57
Abdesalam <abdesalamSemlali@outlook.com>
2023-10-13 02:04:30 +0100

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,222 @@
-import Image from 'next/image'
+"use client";
+import { useState, useEffect } from "react";
 
-export default function Home() {
+export default  function Home() {
+
+  const [price, setPrice] = useState(null);
+  const  [model,setModel] = useState("Binomial")
+  const [option,setOption] = useState("Call")
+  const [underlying, setUnderlying] = useState("")
+  const [strike, setStrike] = useState("")
+  const [interest, setInterest] = useState("")
+  const [volatility, setVolatility] = useState("")
+  const [period, setPeriod] = useState("")
+  const [maturity, setMaturity] = useState(new Date().toISOString().slice(0, 10))
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("https://options-back-end.onrender.com/calculate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          model,
+          option,
+          underlying,
+          strike,
+          interest,
+          volatility,
+          maturity,
+          period,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to send data.");
+      }
+
+      const data = await response.json();
+      setPrice(data.price); // Handle the response data as needed
+    } catch (error) {
+      console.error(error);
+    }
+  };
+    
+    
+    
+
+  
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+    <main className='px-28 py-14 flex gap-52'>
+
+      <div className='flex flex-col w-[40%]'>
+        <h1 className="w-[415.31px] text-black text-5xl font-bold text-left leading-[52.56px] mb-14">Option Calculator</h1>
+
+        <div className='flex flex-col space-y-4 mb-6'>
+          <h3 className='font-bold text-2xl text-black leading-relaxed'>Model</h3>
+          <div className="text-neutral-800 text-base font-light leading-[17.52px]">Choose the model used to calculate the option</div>
+          <div className='flex'>
+            <div className={`w-1/3 h-[58px] px-[38px] py-5 ${model === "Binomial" ? "bg-zinc-800 rounded-lg  shadow-inner font-medium text-white" : "border border-stone-300 font-light text-black"}   flex justify-center items-center gap-2 text-base  leading-[17.52px] cursor-pointer`}
+            onClick={(e)=>{
+                setModel(e.target.innerText)
+            }}
+            >
+              Binomial
+            </div>
+            <div className={`w-1/3 h-[58px] px-[38px] py-5 border ${model === "Trinomial" ? "bg-zinc-800 rounded-lg  shadow-inner font-medium text-white" : "border border-stone-300 font-light text-black"} flex justify-center items-center gap-2  text-base  leading-[17.52px] cursor-pointer`}
+            onClick={(e)=>{
+                setModel(e.target.innerText)
+            }}
+            >
+              Trinomial
+            </div>
+            <div className={`w-1/3 h-[58px] px-[38px] py-5 ${model === "Black&Scholes" ? "bg-zinc-800 rounded-lg  shadow-inner font-medium text-white" : "border border-stone-300 font-light text-black"} flex justify-center items-center gap-2  text-base  leading-[17.52px] cursor-pointer`}
+            onClick={(e)=>{
+                setModel(e.target.innerText)
+            }}
+            >
+              Black&Scholes
+            </div>
+          </div>
+          <div className="border-b-2 w-full  bg-[#E4E4E7] "></div>
         </div>
+
+        <div className='flex flex-col space-y-4 mb-6'>
+          <h3 className='font-bold text-2xl text-black leading-relaxed'>Option Type</h3>
+          <div className="text-neutral-800 text-base font-light leading-[17.52px]">Choose the model used to calculate the option</div>
+          <div className='flex'>
+            <div className={`w-1/2 h-[58px] px-[38px] py-5 ${option === "Call" ? "bg-zinc-800 rounded-lg  shadow-inner font-medium text-white" : "border border-stone-300 font-light text-black"} flex justify-center items-center gap-2  text-base  leading-[17.52px] cursor-pointer`}
+            onClick={(e)=>{
+                setOption(e.target.innerText)
+            }}
+            >
+              Call
+            </div>
+            <div className={`w-1/2 h-[58px] px-[38px] py-5 ${option === "Put" ? "bg-zinc-800 rounded-lg  shadow-inner font-medium text-white" : "border border-stone-300 font-light text-black"}  flex justify-center items-center gap-2  text-base  leading-[17.52px] cursor-pointer`}
+            onClick={(e)=>{
+                setOption(e.target.innerText)
+            }}
+            >
+              Put
+            </div>
+          </div>
+          <div className="border-b-2 w-full  bg-[#E4E4E7] "></div>
+        </div>
+
+        <div className='flex flex-col space-y-4 mb-6'>
+          <h3 className='font-bold text-2xl text-black leading-relaxed'>Prix du sous jacent</h3>
+          <div className="text-neutral-800 text-base font-light leading-[17.52px]">Choose the model used to calculate the option</div>
+          <input type="number" value={underlying}  placeholder="$ 145.00" className="px-4 py-5 h-[58px] placeholder:text-zinc-500  rounded-lg border border-stone-300 justify-start number-input items-center" 
+            onChange={(e)=>{
+              setUnderlying(e.target.value)
+            }}
+          />
+          <div className="border-b-2 w-full  bg-[#E4E4E7] "></div>
+        </div>
+
+        <div className='flex flex-col space-y-4 mb-6'>
+          <h3 className='font-bold text-2xl text-black leading-relaxed'>Prix d'exercice</h3>
+          <div className="text-neutral-800 text-base font-light leading-[17.52px]">Choose the model used to calculate the option</div>
+          <input type="number" value={strike} placeholder="$ 10.00" className="px-4 py-5 h-[58px] placeholder:text-zinc-500  rounded-lg border border-stone-300 justify-start number-input items-center" 
+            onChange={(e)=>{
+              setStrike(e.target.value)
+            }}
+          />
+        </div>
+        
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex flex-col w-[40%]">
+        <div className='flex flex-col space-y-4 mb-6'>
+          <h3 className='font-bold text-2xl text-black leading-relaxed'>Taux d'intérêt</h3>
+          <div className="text-neutral-800 text-base font-light leading-[17.52px]">Choose the model used to calculate the option</div>
+          <input type="number" value={interest} placeholder="% 5.00" className="px-4 py-5 h-[58px] placeholder:text-zinc-500  rounded-lg border border-stone-300 justify-start number-input items-center" 
+            onChange={(e)=>{
+              setInterest(e.target.value)
+            }}
+          />
+          <div className="border-b-2 w-full  bg-[#E4E4E7] "></div>
+        </div>
+
+        <div className='flex flex-col space-y-4 mb-6'>
+          <h3 className='font-bold text-2xl text-black leading-relaxed'>Volatilité</h3>
+          <div className="text-neutral-800 text-base font-light leading-[17.52px]">Choose the model used to calculate the option</div>
+          <input type="number" value={volatility} placeholder="% 3.00" className="px-4 py-5 h-[58px] placeholder:text-zinc-500 rounded-lg border border-stone-300 justify-start number-input items-center" 
+            onChange={(e)=>{
+              setVolatility(e.target.value)
+            }}
+          />
+          <div className="border-b-2 w-full  bg-[#E4E4E7] "></div>
+        </div>
+
+        <div className='flex flex-col space-y-4 mb-6'>
+          <h3 className='font-bold text-2xl text-black leading-relaxed'>Maturité</h3>
+          <div className="text-neutral-800 text-base font-light leading-[17.52px]">Choose the model used to calculate the option</div>
+          <input type="date" value={maturity} className="px-4 py-5 h-[58px] rounded-lg text-zinc-500 border border-stone-300 justify-start number-input items-center" 
+            onChange={(e)=>{
+              setMaturity(e.target.value)
+            }}
+          />
+        </div>
+
+        {
+          (model === "Binomial" || model === "Trinomial") ? (
+            <div className='flex flex-col space-y-4 mb-6'>
+              <h3 className='font-bold text-2xl text-black leading-relaxed'>Nombre de Période</h3>
+              <div className="text-neutral-800 text-base font-light leading-[17.52px]">Choose the model used to calculate the option</div>
+              <input
+                type="number"
+                placeholder="10"
+                value={period}
+                className="px-4 py-5 h-[58px] rounded-lg text-zinc-500 border border-stone-300 justify-start number-input items-center"
+                onChange={(e) => {
+                  setPeriod(e.target.value);
+                }}
+              />
+            </div>
+          ) : null
+        }
+        
+
+        <div className=" flex space-x-6 justify-center items-end grow-[0.90]  "> 
+          <button className="text-zinc-800 text-base py-5 font-bold  underline leading-[17.52px]"
+          onClick={()=>{
+            setMaturity(new Date().toISOString().slice(0, 10))
+            setVolatility("")
+            setInterest("")
+            setStrike("")
+            setUnderlying("")
+            setPeriod("")
+            setPrice(null)
+          }}
+          >Clear All</button>
+          <button className="grow basis-0 shrink h-[58px] flex px-4 py-5 bg-black text-white rounded-lg justify-center items-center"
+          onClick={handleSubmit}
+          >Calculate</button>
+        </div>
+        <div className="items-center justify-center">{price}</div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      
     </main>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
